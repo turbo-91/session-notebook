a266c72337ce97c86bfb7c7f1517d7cded820032
Torben Jost <torbenjost@proton.me>
2024-04-12 15:19:55 +0200

    session notes

---
+++ javascript/js_functions/notes.js
@@ -0,0 +1,14 @@
+//declaring functions
+// parameters
+// scope
+
+// declaring ("creating") a function:
+function display(name, age) {
+  // here name is called a parameter (to the function), side note: it is more elegant to have as few parameters in a function as poss
+  console.log(name, age);
+}
+
+// calling a function:
+display("sven", 49);
+// this is an argument
+display("jan");
